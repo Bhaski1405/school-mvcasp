cd6d17ff4a2c043c9c660b0d82361983c62c29c6
kneerunjun <kneerunjun@outlook.com>
2015-04-10 16:55:32 +0530

    simple table delivred from the server

---
+++ AcademyApp/AcademyApp/Controllers/EmployeesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AcademyApp.Models;
+
+namespace AcademyApp.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        //
+        // GET: /Employees/
+
+        public ActionResult Index()
+        {
+            EmployeeIndex vm = new EmployeeIndex() {
+                Names = new List<string>(){
+                    "Niranjan",
+                    "Pragya",
+                    "Arun",
+                    "Paul",
+                    "Maharaja",
+                    "Avinash"
+                }
+            };
+            return View("Index", vm);
+        }
+
+    }
+}
