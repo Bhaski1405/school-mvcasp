3c4c3079819e908918433a50bdb643f2d3352fd6
kneerunjun <kneerunjun@outlook.com>
2015-04-28 16:05:15 +0530

    simple demo problems for th controller and action

---
+++ AcademyApp/AcademyApp/Models/GreetModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AcademyApp.Models
+{
+    public class GreetModel
+    {
+        public string Greeting { get; set; }
+    }
+}
---
+++ AcademyApp/AcademyApp/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AcademyApp.Models
+{
+    public class UserModel
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Project { get; set; }
+    }
+}
